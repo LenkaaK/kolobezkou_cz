b047ab5e7467ccc5dfefb1073632dd9af5d542a9
veronicie <veronicie@seznam.cz>
2020-06-19 00:35:16 +0200

    Lehký pokrok ve zobrazení detailu trasy

--- src/views/Seznamka.vue
+++ src/views/Seznamka.vue
@@ -4,25 +4,43 @@
     <div class="trasapopis">
       <h2>{{trasy[0].name}}</h2>
       <p>Hodnocení: {{trasy[0].stars}}</p>
-      <p>{{trasy[0].distance}} km / {{trasy[0].time}} hodin</p>
+
+      <div class="inline">
+        <div><img src="./../assets/icons/distance01.svg" width="40px"></div>
+        <div><p>{{trasy[0].distance}} km / {{trasy[0].time}} hodin</p></div>
+      </div>
       <p>{{trasy[0].ascent}} m</p>
 
-      <p
-      v-for="(trasa, index) in trasy[0].region"
-      v-bind:key="index"
-      class="inline"
-      >
-        {{trasa}}
-      </p>
 
-      <p
-      v-for="(trasa, index) in trasy[0].hashtags"
-      v-bind:key="index"
-      class="inline"
-      >
-        #{{trasa}}
-      </p>
+      <div>
+        <p
+        v-for="(trasa, index) in trasy[0].region"
+        v-bind:key="index"
+        class="inline"
+        >
+          {{trasa}}
+        </p>
+      </div>
+
+      <div>
+        <p
+        v-for="(trasa, index) in trasy[0].hashtags"
+        v-bind:key="index"
+        class="inline"
+        >
+          #{{trasa}}
+        </p>
+      </div>
     </div>
+        
+      <!-- ZKOUŠKA GRAFU -->
+    <figure class="pie-chart">
+      	<figcaption>
+		      Terén 5%<span style="color:#59a14f"></span><br>
+		      Asfalt 95%<span style="color:#edc949"></span>
+	      </figcaption>
+    </figure>
+
   </div>
 </template>
 
@@ -46,6 +64,12 @@ export default {
     text-align: left;
   }
 
+  .trasapopis {
+    margin-left: 15px;
+    margin-right: 15px;
+    max-width: 50%;
+  }
+
   .mapa {
     text-align: center;
     margin: 20px;
@@ -54,4 +78,47 @@ export default {
   .inline {
     display: inline;
   }
+
+  /* STYLOVÁNÍ GRAFU */
+
+  .pie-chart {
+    background:
+      radial-gradient(
+        circle closest-side,
+        white 0,
+        white 39.6%,
+        transparent 39.6%,
+        transparent 60%,
+        white 0
+      ),
+      conic-gradient(
+        #59a14f 0,
+        #59a14f 5%,
+        #edc949 0,
+        #edc949 100%
+    );
+    position: relative;
+    max-width: 500px;
+    min-height: 350px;
+    margin: 0;
+    /*outline: 1px solid #ccc;*/
+  }
+  .pie-chart figcaption {
+    position: absolute;
+    bottom: 45%;
+    right: 42%;
+    font-size: smaller;
+    text-align: center;
+  }
+  .pie-chart span:after {
+    display: inline-block;
+    content: "";
+    width: 0.8em;
+    height: 0.8em;
+    margin-left: 0.4em;
+    height: 0.8em;
+    border-radius: 0.2em;
+    background: currentColor;
+  }
+
 </style>
