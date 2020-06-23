62e28fd06bd3100f23186917d8c1861b344560f4
LenkaaK <64088404+LenkaaK@users.noreply.github.com>
2020-06-23 08:50:40 +0200

    doplnění půjčovny

--- src/assets/data/pujcovny.js
+++ src/assets/data/pujcovny.js
@@ -1,46 +1,98 @@
 import { latLng } from "leaflet"
 export default [
     {
-    
+
         name: 'Svět koloběžek',
         url: 'https://www.svetkolobezek.cz/',
         gps: latLng(49.96559, 14.07355),
     },
-    
+
     {
-    
+
         name: 'Hotel Skalní mlýn',
         url: ' www.skalnimlyn.cz',
         gps: latLng(49.3637673, 16.7082619),
-      
+
     },
     {
-    
+
         name: 'Koloběžky Brandýs',
         url: 'https://kolobezky-brandys.cz/cs',
         gps: latLng(50.1877414, 14.6650040),
-        
+
     },
     {
-    
+
         name: 'kolobezky.bike',
         url: 'http://www.kolobezky.bike/',
         gps: latLng(50.1553839, 17.0146693),
-        
+
     },
     {
-    
+
         name: 'Kolobky.cz',
         url: 'https://kolobky.cz/pujcovna/',
         gps: latLng(49.1893020, 16.6051142),
-        
+
     },
     {
-    
+
         name: 'Kostka-kolobka',
         url: 'https://www.kostkakolobezky.cz/',
         gps: latLng(50.0781549, 16.9591203),
+
+    },
+    {   name: 'Koloběžky Ostrava',
+        url: 'https://www.kolobezkarna.cz/',
+        gps: latLng(49.8780178, 18.2599678), },
+
+    {   name: 'Koloběžíme',
+        url: 'http://kolobezime.cz/info/7-pujcovny', gps: latLng(50.0690445, 14.4148901) 
+    },
         
+    { name: 'Svět koloběžek',
+     url: 'https://www.svetkolobezek.cz/pujcovna-kolobezek/', 
+     gps: latLng(50.0397670, 14.4918094) 
+    }, 
+    { 
+        name: 'Lerko Sport', 
+        url: 'http://www.kolobezky-lipence.cz/pujcovna.html', 
+        gps: latLng(49.9595507, 14.3535383) 
+    }, 
+    { 
+        name: 'VeloRama.cz', 
+        url: 'https://www.velorama.cz/pujcovna/', 
+        gps: latLng(50.1006986, 14.4226771) 
+    }, 
+    {   name: 'S koloběžkou', 
+        url: 'https://www.skolobezkou.cz/', 
+        gps: latLng(49.1587021, 16.8785053) 
+    }, 
+    {   name: 'U koloběžky', 
+        url: 'https://www.ukolobezky.cz/pujcovna-kolobezek/', 
+        gps: latLng(48.9980036, 15.3502545)
+     },
+    {   name: 'JK Kolobka', 
+        url: 'https://kolobkaolomouc.cz/', 
+        gps: latLng(49.6020477, 17.2772736)
+     },
+
+        
+    {   name: 'Loko sport', 
+        url: 'http://lokosport.eu/', 
+        gps: latLng(49.6656503, 15.4012536)
+     },
+    {   name: 'KOLOBĚHsport', 
+        url: 'https://www.kolobezky-plzen.cz/', 
+        gps: latLng(49.7465559, 13.358471) 
     },
 
+    {   name: 'Koloběžky Krumlov', 
+        url: 'https://www.kolobezky-krumlov.cz/', 
+        gps: latLng(48.8652589, 14.2833589)
+     },
+    {   name: 'Adrenalin Park', 
+        url: 'https://www.kolobezky-krumlov.cz/', 
+        gps: latLng(48.8652589, 14.2833589)
+     },
 ]
--- src/views/Kontakty.vue
+++ src/views/Kontakty.vue
@@ -41,7 +41,7 @@ export default {
 <style scoped lang="scss">
 .contact{
   width:70%;
-  margin: 0 auto;
+  margin: 0 auto 120px auto;
 
 }
 .paragrafs{
